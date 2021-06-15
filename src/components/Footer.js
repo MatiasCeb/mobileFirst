b9b4bb0196509c88820b6c70739be623075969cd
MatiasCeb <matiasceb8812@gmail.com>
2021-06-14 21:50:56 -0300

    class 11 complete

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles/Footer.css"
+
+const Footer = () => {
+    return(
+        <div>
+        <p className="footer">Footer</p>
+        <p>asdsadadasdsad</p>
+        </div>
+    )
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles/Header.css";
+
+
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="header--img" alt="" />
+            <div className="header--title-container">
+                <h1 className="header--title-h1">La próxima revolución en el intercambio de criptomonedas.</h1>
+                <p className="header--title-p">Batatabit ayuda a navegar entre los diferentes precios y tendencias.</p>
+                <a href="" className="header--button">Conoce nuestros planes <span className="header--button-span"></span></a>
+            </div>
+        </div>
+    )
+};
+
+export default Header;
