f3f367ff84edd06a7399e05aaafaa7875edab694
MatiasCeb <matiasceb8812@gmail.com>
2021-06-19 00:04:36 -0300

    Netlify 3

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,6 @@ const Header = () => {
             <div className="header--title-container">
                 <h1 className="header--title-h1">La próxima revolución en el intercambio de criptomonedas.</h1>
                 <p className="header--title-p">Batatabit ayuda a navegar entre los diferentes precios y tendencias.</p>
-                <a href="#" className="header--button">Conoce nuestros planes <span className="header--button-span"></span></a>
             </div>
         </div>
     )
