5d16926788890cadc4c8b2ab9d1a470fdcce0970
MatiasCeb <matiasceb8812@gmail.com>
2021-06-27 19:08:38 -0300

    navbar links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,7 @@
 import React from "react";
+import linkedin from "../components/assets/icons/icons8-linkedin-white-30px.png";
+import instagram from "../components/assets/icons/icons8-instagram-white-24px.png";
+import github from "../components/assets/icons/icons8-github-white-30px.png";
 import "./styles/Footer.css"
 
 const Footer = () => {
@@ -6,9 +9,9 @@ const Footer = () => {
         <div className="footer-container">
             <section className="section">
                 <ul>
-                    <li><a href="https://www.instagram.com/matiasceb/" rel="noopener noreferrer" target="_blank">Instagram</a></li>
-                    <li><a href="https://www.linkedin.com/in/matías-ceballos-424001182/" rel="noopener noreferrer" target="_blank">Linkedin</a></li>
-                    <li><a href="https://github.com/MatiasCeb" rel="noopener noreferrer" target="_blank">Github</a></li>
+                    <li><a href="https://www.instagram.com/matiasceb/" rel="noopener noreferrer" target="_blank"><img className="footer--img" src={instagram} alt="Logo" />Instagram</a></li>
+                    <li><a href="https://www.linkedin.com/in/matías-ceballos-424001182/" rel="noopener noreferrer" target="_blank"><img className="footer--img" src={linkedin} alt="Logo" />Linkedin</a></li>
+                    <li><a href="https://github.com/MatiasCeb" rel="noopener noreferrer" target="_blank"><img className="footer--img" src={github} alt="Logo" />Github</a></li>
                 </ul>
             </section>
             <section className="section">
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,23 @@
 import React from "react";
 import "./styles/NavBar.css"
 import logo from "../components/assets/icons/devicon.png";
+import linkedin from "../components/assets/icons/icons8-linkedin-white-30px.png";
+import instagram from "../components/assets/icons/icons8-instagram-white-24px.png";
+import github from "../components/assets/icons/icons8-github-white-30px.png";
 
 class Navbar extends React.Component {
     render() {
         return (
         <div className="Navbar">
-            <div className="container-fluid">
-                <div>
+                <div classNAme="section">
                     <img className="Navbar__brand-logo" src={logo} alt="Logo"/>
                     <span className="Header-name">Matías</span>
                     <span className="Header-lastname">Ceb</span>
                 </div>
+                <div className="section">
+                    <div><a href="https://www.instagram.com/matiasceb/" rel="noopener noreferrer" target="_blank"><img className="navbar--img" src={instagram} alt="Logo" /></a></div>
+                    <div><a href="https://www.linkedin.com/in/matías-ceballos-424001182/" rel="noopener noreferrer" target="_blank"><img className="navbar--img" src={linkedin} alt="Logo" /></a></div>
+                    <div><a href="https://github.com/MatiasCeb" rel="noopener noreferrer" target="_blank"><img className="navbar--img" src={github} alt="Logo" /></a></div>
             </div>
         </div>
         )
