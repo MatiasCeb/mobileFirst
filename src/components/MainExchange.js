d1760f78808af1f4f8c0659746498f48d76c8b0f
MatiasCeb <60543600+MatiasCeb@users.noreply.github.com>
2024-05-08 17:51:51 -0300

    Update MainExchange.js

--- src/components/MainExchange.js
+++ src/components/MainExchange.js
@@ -12,7 +12,7 @@ const MainExchange = () => {
         <section className="main-exchange-container">
             <div className="main-exchange-container--title">
                 <h2 className="main-exchange-container--h2">Who am I?</h2>
-                <p className="main-exchange-container--p">An experienced project manager with knowledge on react and javascript with a mix of soft skill.</p>
+                <p className="main-exchange-container--p">An experienced project manager with knowledge on Java, React and Javascript with a mix of soft skill.</p>
             </div>
             <div className="backgroundImg"></div>
             <img className="mainexchange--img" src={logo} alt="Logo" />
